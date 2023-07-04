c8bb62c02697d7aeb50788a94fdc51d3ddc320db
Samuel Nunes <samuelluiznunes10@gmail.com>
2023-07-04 14:54:42 -0300

    anotacoes

--- src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/TesteWeb.java
@@ -2,25 +2,35 @@ package br.com.chronosAcademy.automacaoWeb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteWeb {
+    ChromeDriver driver;
+
+    @Before
+    public void inicializaTeste(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.chronosacademy.com.br/");
+    }
+
+    @After
+    public void finalizaTeste(){
+        driver.quit();
+    }
 
     @Test
     public void primeiroTeste(){
-        WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.chronosacademy.com.br/");
-
         String xpathTitulo = "//h4[contains(.,'Porque tempo é conhecimento.')]";
         WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
         String titulo = txtTitulo.getText();
         assertEquals("Porque Tempo É Conhecimento.", titulo);
-
-        driver.quit();
     }
 }
